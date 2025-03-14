1095479d98f41f12ae895610888149a1e4079893
IVGos <greengesty@gmail.com>
2025-03-14 19:59:13 +0300

    fix

--- client/src/app/router/router.tsx
+++ client/src/app/router/router.tsx
@@ -1,7 +1,7 @@
 
 import { JSX } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { MainPage, NotFoundPage } from "@/pages";
+import {  NotFoundPage } from "@/pages";
 import { CLIENT_ROUTES } from "@/shared/enums/clientRoutes";
 import Layout from "../layout/Layout";
 import { SignUpPage } from "@/pages/SignUpPage/SignUpPage";
