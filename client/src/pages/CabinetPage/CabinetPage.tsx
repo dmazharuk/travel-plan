33401fb1c51c6703748e3486a0bd97cdc3a18f1e
sashamoramova <morakovaaleksandra1@gmail.com>
2025-03-14 21:36:24 +0300

    stylesforms

--- client/src/pages/CabinetPage/CabinetPage.tsx
+++ client/src/pages/CabinetPage/CabinetPage.tsx
@@ -1,10 +1,11 @@
 // import { FriendsRoutes } from "@/widgets/FriendsRoutes/FriendsRoutes";
 import { MyRoads } from "@/widgets/MyRoads/MyRoads";
+import styles from "./CabinetPage.module.css";
 
 
 export default function CabinetPage() {
   return (
-    <div><h2>CabinetPage</h2>
+    <div className={styles.main}>
       <MyRoads />
       {/* <FriendsRoutes/> */}
     </div>
