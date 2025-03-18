da118385868bfd4aa25ff4c34a1302ccece27603
sashamoramova <morakovaaleksandra1@gmail.com>
2025-03-18 15:45:55 +0300

    Version for commit

--- client/src/features/map/ui/MapViewer/MapViewer.tsx
+++ client/src/features/map/ui/MapViewer/MapViewer.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from "react";
 // import styles from "./MapViewer.module.css";
 //for commit
+//for commit
+
 
 interface MapViewerProps {
   points: { coords: [number, number]; name: string; number: number }[];
