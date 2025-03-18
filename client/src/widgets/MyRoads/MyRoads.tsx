340c417de97444a4a5cc6a9fbf327cfa9b555ee2
IVGos <greengesty@gmail.com>
2025-03-18 22:02:41 +0300

    fix name companion

--- client/src/widgets/MyRoads/MyRoads.tsx
+++ client/src/widgets/MyRoads/MyRoads.tsx
@@ -1,8 +1,8 @@
-import { getAllRoads, updateRoad } from "@/app/entities/road";
-import { useAppDispatch, useAppSelector } from "@/shared/hooks/reduxHooks";
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-import styles from "./MyRoads.module.css";
+import { getAllRoads, updateRoad } from '@/app/entities/road';
+import { useAppDispatch, useAppSelector } from '@/shared/hooks/reduxHooks';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import styles from './MyRoads.module.css';
 
 export function MyRoads() {
   const dispatch = useAppDispatch();
@@ -16,7 +16,12 @@ export function MyRoads() {
   }, [dispatch]);
 
   // проверка на наличие author
-  const userRoads = roads.filter((road) => road.author?.id === user?.id || road.companions?.some((companion) => companion.id === user?.id) && road.visibility === "public");
+  const userRoads = roads.filter(
+    (road) =>
+      road.author?.id === user?.id ||
+      (road.companions?.some((companion) => companion.id === user?.id) &&
+        (road.visibility === 'public' || road.visibility === 'friends')),
+  );
 
   const getRandomColor = () => {
     const hue = Math.floor(Math.random() * 360);
@@ -25,18 +30,25 @@ export function MyRoads() {
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
   };
 
+  const getNextVisibility = (currentVisibility: 'private' | 'friends' | 'public') => {
+    if (currentVisibility === 'public') return 'friends';
+    if (currentVisibility === 'friends') return 'private';
+    return 'public';
+  };
+
   const handleToggleVisibility = (
     roadId: number,
-    currentVisibility: "private" | "friends" | "public"
+    currentVisibility: 'private' | 'friends' | 'public',
   ) => {
-    const newVisibility = currentVisibility === "public" ? "private" : "public";
+    const newVisibility = getNextVisibility(currentVisibility);
+    // const newVisibility = currentVisibility === "public" ? "private" : "public";
 
     const roadToUpdate = roads.find((road) => road.id === roadId);
 
     if (roadToUpdate) {
       const updatedRoadData = {
         ...roadToUpdate,
-        visibility: newVisibility as "private" | "friends" | "public",
+        visibility: newVisibility as 'private' | 'friends' | 'public',
         city: roadToUpdate.city,
         country: roadToUpdate.country,
         transport: roadToUpdate.transport,
@@ -45,7 +57,7 @@ export function MyRoads() {
       };
 
       dispatch(updateRoad({ id: roadId, roadData: updatedRoadData })).then(() =>
-        dispatch(getAllRoads())
+        dispatch(getAllRoads()),
       );
     }
   };
@@ -55,7 +67,7 @@ export function MyRoads() {
   };
 
   const handleCreateRoadClick = () => {
-    navigate("/create-road");
+    navigate('/create-road');
   };
 
   if (isLoading) return <div>Loading...</div>;
@@ -67,10 +79,7 @@ export function MyRoads() {
         <h1 className={styles.title}>Мой кабинет</h1>
         <div className={styles.conteinerForTitle}>
           <h3>Мои маршруты</h3>
-          <button
-            onClick={handleCreateRoadClick}
-            className={styles.createButton}
-          >
+          <button onClick={handleCreateRoadClick} className={styles.createButton}>
             <span>+</span>
             <span className={styles.tooltip}>Создать маршрут</span>
           </button>
@@ -78,14 +87,10 @@ export function MyRoads() {
       </div>
 
       {userRoads.length === 0 ? (
-        <div className={styles.emptyMessage}>
-          У вас пока нет созданных маршрутов
-        </div>
+        <div className={styles.emptyMessage}>У вас пока нет созданных маршрутов</div>
       ) : (
         <div className={styles.roadList}>
           {userRoads.map((road) => (
-           
-          
             <div
               key={road.id}
               className={styles.roadItem}
@@ -98,27 +103,42 @@ export function MyRoads() {
                     {road.city}, {road.country}
                   </h3>
                   <p className={styles.roadDate}>
-                    Создан: {new Date(road.createdAt).toLocaleDateString()} 
-                    <br/>
+                    Создан: {new Date(road.createdAt).toLocaleDateString()}
+                    <br />
                     Создатель: {road.author?.username}
                   </p>
                   <p className={styles.tripDate}>
-                    Даты путешествия: {`${new Date(road.tripStartDate).toLocaleDateString()} - ${new Date(road.tripEndDate).toLocaleDateString()}`}
+                    Даты путешествия:{' '}
+                    {`${new Date(road.tripStartDate).toLocaleDateString()} - ${new Date(road.tripEndDate).toLocaleDateString()}`}
                   </p>
                 </div>
                 {road.author?.id === user?.id && (
-                <button
-                  
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleToggleVisibility(road.id, road.visibility);
-                  }}
-                  className={`${styles.visibilityButton} ${
-                    road.visibility === "public" ? styles.public : ""
-                  }`}
-                >
-                  {road.visibility === "public" ? "Публичный" : "Приватный"}
-                </button>
+                  // <button
+
+                  //   onClick={(e) => {
+                  //     e.stopPropagation();
+                  //     handleToggleVisibility(road.id, road.visibility);
+                  //   }}
+                  //   className={`${styles.visibilityButton} ${
+                  //     road.visibility === "public" ? styles.public : ""
+                  //   }`}
+                  // >
+                  //   {road.visibility === "public" ? "Публичный" : "Приватный"}
+                  // </button>
+
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleToggleVisibility(road.id, road.visibility);
+                    }}
+                    className={`${styles.visibilityButton} }`}
+                  >
+                    {road.visibility === 'public'
+                      ? 'Публичный'
+                      : road.visibility === 'friends'
+                        ? 'Для друзей'
+                        : 'Приватный'}
+                  </button>
                 )}
               </div>
             </div>
@@ -128,4 +148,3 @@ export function MyRoads() {
     </div>
   );
 }
-
