4ef3ea040ec6b4f052ec82dd0a0e6fff68c9218d
dmazharuk <dholoshina1812@gmail.com>
2025-03-20 13:15:22 +0300

    stylesMain

--- client/src/pages/MainPage/MainPage.tsx
+++
@@ -1,7 +0,0 @@
-import { JSX } from 'react';
-import styles from './MainPage.module.css';
-
-export function MainPage(): JSX.Element {
-  return <div className={styles.bebratest} >
-    MainPage</div>;
-}
